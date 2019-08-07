d04c698a91b1400866d10fc6ba58cca92540063c
PAYDUNYA <abdoulayekeita438@gmail.com>
2019-08-07 14:09:08 +0000

    070820191409

--- storage/cms/cache/38/f1/header.htm.php
+++ storage/cms/cache/38/f1/header.htm.php
@@ -1,5 +1,8 @@
 <?php 
-class Cms5d495f27b5e8f435940401_a7b3ba104b765cb37651457c3a748fe3Class extends Cms\Classes\PartialCode
+use event\event\models\NotifDemande;class Cms5d4adaff5046a256161268_b750f5cadda7413752ea827e210e2178Class extends Cms\Classes\PartialCode
 {
 
+public function onStart(){
+    $this['notifs']=NotifDemande::all();
+}
 }
